0b8cb980d6c44788d710d76462de46d08f1caae3
BryanBooij <bryan.booij@xs4all.nl>
2020-06-16 09:58:34 +0200

    .

--- indexdesign.php
+++ indexdesign.php
@@ -33,18 +33,6 @@
 </form>
 
 
-
-<script>
-    function openCity(cityName) {
-        var i;
-        var x = document.getElementsByClassName("city");
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = "none";
-        }
-        document.getElementById(cityName).style.display = "block";
-    }
-</script>
-
 <?php
 
 
